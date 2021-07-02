49fcb59d70dd8329928004b3649c9045ff4e2c36
tkiyo86 <32362106+tkiyo86@users.noreply.github.com>
2021-07-02 16:01:28 +0900

    18_const,let

--- src/index.js
+++ src/index.js
@@ -1,10 +1,55 @@
-import "./styles.css";
+/*
+const.letの変数宣言
+*/
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+/**　自動で下記は入力される
+ *
+ */
+
+// ctrl + / でまとめてコメントアウト
+
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き"
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言"
+// console.log(val1);
+
+let val2 = "let変数";
+console.log(val2);
+
+//　上書きが可能
+val2 = "let変数を上書き";
+console.log(val2);
+
+//　letは再宣言できない
+//let val2 = "let変数を再宣言"
+
+const val3 = "const変数";
+console.log(val3);
+
+//const変数は上書き不可
+//val3 = "const変数を再宣言"
+// TypeError: "val3" is read-only
+
+// objや配列は書き換えが可能
+const val4 = {
+  name: "たろう",
+  age: 28
+};
+
+console.log(val4);
+
+val4.name = "はなこ";
+val4.address = "Tokyo";
+console.log(val4);
+
+//　配列の書き換え可能
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monlkey");
+console.log(val5);
